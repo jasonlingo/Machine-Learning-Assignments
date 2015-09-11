eef2d9558bc74d2aa6c7f70eac1a296f12fabd94
Li-Yi Lin <jasonlingo@gmail.com>
2015-09-11 09:48:35 -0400

    FeatureVector.java finished

--- HW_1_Learning Foundations/cs475/FeatureVector.java
+++ HW_1_Learning Foundations/cs475/FeatureVector.java
@@ -1,17 +1,46 @@
 package cs475;
 
-import java.io.Serializable;
+/**
+ * @author: Li-Yi Lin
+ */
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class FeatureVector implements Serializable {
+public class FeatureVector {
+
+	// Feature vector.
+	private Map<Integer, Double> featureVector;
+
+
+	/**
+	 * Construct a feature vector.
+	 */
+	public FeatureVector(){
+		this.featureVector = new HashMap<>();
+	}
 
 	public void add(int index, double value) {
 		// TODO Auto-generated method stub
-		
+		this.featureVector.put(index, value);
 	}
 	
 	public double get(int index) {
 		// TODO Auto-generated method stub
-		return 0;
+		return this.featureVector.get(index);
 	}
 
+	public int size() {
+		// TODO Auto-generated method stub
+		return this.featureVector.size();
+	}
+
+	/**
+	 * Create an iterator for the feature vector.
+	 * @return
+	 */
+	public Iterator iterator(){
+		Iterator it = featureVector.entrySet().iterator();
+		return it;
+	}
 }
