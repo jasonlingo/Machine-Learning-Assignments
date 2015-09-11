8480a26a1e4a2be5966c23818ee918151c203f4e
Li-Yi Lin <jasonlingo@gmail.com>
2015-09-11 10:15:37 -0400

    Finished ClassificationLabel.java and RegressionLabel.java

--- HW_1_Learning Foundations/cs475/ClassificationLabel.java
+++ HW_1_Learning Foundations/cs475/ClassificationLabel.java
@@ -4,14 +4,17 @@ import java.io.Serializable;
 
 public class ClassificationLabel extends Label implements Serializable {
 
+	int label;
+
 	public ClassificationLabel(int label) {
 		// TODO Auto-generated constructor stub
+		this.label = label;
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return Integer.toString(this.label);
 	}
 
 }
--- HW_1_Learning Foundations/cs475/RegressionLabel.java
+++ HW_1_Learning Foundations/cs475/RegressionLabel.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 
 public class RegressionLabel extends Label implements Serializable {
 
+
+	double label;
+
 	public RegressionLabel(double label) {
 		// TODO Auto-generated constructor stub
+		this.label = label;
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return Double.toString(this.label);
 	}
 
 }
