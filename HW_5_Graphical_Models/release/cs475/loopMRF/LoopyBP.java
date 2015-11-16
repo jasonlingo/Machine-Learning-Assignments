7944219168f272cfc2025d0670be1faa011fc6c4
Li-Yi Lin <jasonlingo@gmail.com>
2015-11-16 13:05:06 -0500

    HW5 init

---
+++ HW_5_Graphical_Models/release/cs475/loopMRF/LoopyBP.java
@@ -0,0 +1,20 @@
+
+package cs475.loopMRF;
+
+public class LoopyBP {
+
+	private LoopMRFPotentials potentials;
+	private int iterations;
+	// add whatever data structures needed
+
+	public LoopyBP(LoopMRFPotentials p, int iterations) {
+		this.potentials = p;
+		this.iterations = iterations;
+	}
+
+	public double[] marginalProbability(int x_i) {
+		// TODO
+	}
+
+}
+
